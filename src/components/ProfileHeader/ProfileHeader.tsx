e765d9e3bf5905ad6c98f4dbb3b49c98c57d055a
fiftysev <nesgorai@bk.ru>
2021-10-14 18:35:47 +1000

    Fix invisibility for undefined props

--- src/components/ProfileHeader/ProfileHeader.tsx
+++ src/components/ProfileHeader/ProfileHeader.tsx
@@ -26,15 +26,15 @@ const ProfileHeader = (props: ProfileHeaderProps) => (
             <span className={styles.description}> {props.description} </span>
 
             <div className={styles.additional_info}>
-                <span className={`${styles.country} ${props.locationCountry?.length === 0 ? styles.invisible : styles.visible}`}>
+                {props.locationCountry && <span className={styles.country}>
                     <svg viewBox="0 0 24 24" aria-hidden="true"
                          className="r-9ilb82 r-4qtqp9 r-yyyyoo r-1xvli5t r-1d4mawv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><path
     d="M12 14.315c-2.088 0-3.787-1.698-3.787-3.786S9.913 6.74 12 6.74s3.787 1.7 3.787 3.787-1.7 3.785-3.787 3.785zm0-6.073c-1.26 0-2.287 1.026-2.287 2.287S10.74 12.814 12 12.814s2.287-1.025 2.287-2.286S13.26 8.24 12 8.24z"/><path
     d="M20.692 10.69C20.692 5.9 16.792 2 12 2s-8.692 3.9-8.692 8.69c0 1.902.603 3.708 1.743 5.223l.003-.002.007.015c1.628 2.07 6.278 5.757 6.475 5.912.138.11.302.163.465.163.163 0 .327-.053.465-.162.197-.155 4.847-3.84 6.475-5.912l.007-.014.002.002c1.14-1.516 1.742-3.32 1.742-5.223zM12 20.29c-1.224-.99-4.52-3.715-5.756-5.285-.94-1.25-1.436-2.742-1.436-4.312C4.808 6.727 8.035 3.5 12 3.5s7.192 3.226 7.192 7.19c0 1.57-.497 3.062-1.436 4.313-1.236 1.57-4.532 4.294-5.756 5.285z"/>
                     </svg>
                     {props.locationCountry}
-                </span>
-                <span className={`${styles.website} ${props.websiteLink?.length === 0 ? styles.invisible : styles.visible}`}>
+                </span>}
+                {props.websiteLink && <span className={styles.website}>
                     <svg viewBox="0 0 24 24" aria-hidden="true"
                          className="r-9ilb82 r-4qtqp9 r-yyyyoo r-1xvli5t r-1d4mawv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr">
                         <path
@@ -42,8 +42,8 @@ const ProfileHeader = (props: ProfileHeaderProps) => (
     d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"/>
                          </svg>
                     <a href="google.com">{props.websiteLink}</a>
-                </span>
-                <span className={`${styles.joined_date} ${props.dateJoined?.length === 0 ? styles.invisible : styles.visible}`}>
+                </span>}
+                {props.dateJoined  && <span className={styles.joined_date}>
                     <svg viewBox="0 0 24 24" aria-hidden="true"
                          className="r-9ilb82 r-4qtqp9 r-yyyyoo r-1xvli5t r-1d4mawv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr">
                         <path
@@ -57,7 +57,7 @@ const ProfileHeader = (props: ProfileHeaderProps) => (
     r="1.285"/>
                     </svg>
                     Регистрация: {props.dateJoined}
-                </span>
+                </span>}
             </div>
             <div className={styles.followers_info}>
                 <span className={styles.following}><b>{props.followingCount}</b> в читаемых</span>
