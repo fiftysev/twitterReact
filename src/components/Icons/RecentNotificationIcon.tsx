f7cc0a6532106290249be046877754770d45376b
fiftysev <nesgorai@bk.ru>
2021-11-02 12:49:59 +1000

    Minor fix Add some icons for notifications page, delete unused files

---
+++ src/components/Icons/RecentNotificationIcon.tsx
@@ -0,0 +1,14 @@
+const RecentNotificationIcon = () => (
+  <svg
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    style={{ color: "rgb(29, 155, 240)" }}
+  >
+    <path
+      d="m22.99 11.295-6.986-2.13-.877-.326-.325-.88L12.67.975a.72.72 0 0 0-1.376 0l-2.392 7.84-1.774.657-6.148 1.82a.72.72 0 0 0 0 1.38l7.956 2.358 2.356 7.956a.719.719 0 0 0 1.38.001l1.822-6.15.656-1.773 7.84-2.392a.72.72 0 0 0 0-1.375z"
+      fill="#794BC4"
+    />
+  </svg>
+);
+
+export default RecentNotificationIcon;
