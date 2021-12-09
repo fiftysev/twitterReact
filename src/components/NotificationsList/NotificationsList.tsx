6aa2ffc655c1de285ad36bf9d58639ea19d299d9
fiftysev <nesgorai@bk.ru>
2021-12-09 21:25:33 +1000

    Minor updates

--- src/components/NotificationsList/NotificationsList.tsx
+++ src/components/NotificationsList/NotificationsList.tsx
@@ -5,31 +5,31 @@ import data from "../../replyNotificationsData.json";
 import ReplyNotification from "./ReplyNotification";
 
 const NotificationsList = () => (
-  <div className={styles.list_container}>
-    {notificationsData.map((e) => (
-      <Notification
-        notificationType={e.notificationType}
-        notificationAuthor={e.notificationAuthor}
-        notificationAuthorAvatar={e.notificationAuthorAvatar}
-        notificationText={e.notificationText}
-      />
-    ))}
-    {data.map((e) => (
-      <ReplyNotification
-        authorAvatar={e.avatar}
-        authorName={e.name}
-        authorUsername={e.username}
-        repliedTimeAgo={e.timeAgo}
-        replyText={e.text}
-        replyImage={e.image}
-        commentsCount={e.comments}
-        retweetsCount={e.retweets}
-        likesCount={e.likes}
-        isLiked={e.liked}
-        replyTo={e.replyTo}
-      />
-    ))}
-  </div>
+    <div className={styles.list_container}>
+        {notificationsData.map((e) => (
+            <Notification
+                notificationType={e.notificationType}
+                notificationAuthor={e.notificationAuthor}
+                notificationAuthorAvatar={e.notificationAuthorAvatar}
+                notificationText={e.notificationText}
+            />
+        ))}
+        {data.map((e) => (
+            <ReplyNotification
+                authorAvatar={e.avatar}
+                authorName={e.name}
+                authorUsername={e.username}
+                repliedTimeAgo={e.timeAgo}
+                replyText={e.text}
+                replyImage={e.image}
+                commentsCount={e.comments}
+                retweetsCount={e.retweets}
+                likesCount={e.likes}
+                isLiked={e.liked}
+                replyTo={e.replyTo}
+            />
+        ))}
+    </div>
 );
 
 export default NotificationsList;
